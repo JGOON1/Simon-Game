3a997f4b549b9c8dd87f4935c3b0fc4b61f9577b
john Meng <john.meng014@gmail.com>
2019-09-03 20:56:51 -0400

    added function to slect random color from array

--- game.js
+++ game.js
@@ -1,13 +1,40 @@
 $(document).ready(function(){
 
-
-
-
-
-
-
-
-
-
+    var buttonColours = ["red","blue","green","yellow"];
+    var gamePattern = [];
+    var userClickedPattern = [];
     
+    $(".btn").click(function(){
+        var userChosenColour = $(this).attr("id");
+        userClickedPattern.push(userChosenColour);
+        console.log(userClickedPattern);
+
+
+
+    });
+
+
+    function nextSequence(){
+        var randomNumber = Math.floor(Math.random() * 4);
+        var randomChosenColor = buttonColours[randomNumber];
+        gamePattern.push(randomChosenColor);
+
+        $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+        var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+        audio.play();  
+
+
+
+    };
+
+
+
+
+
+
+
+
+
+
 })
